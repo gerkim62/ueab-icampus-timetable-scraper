22800a53a10c721c85b59e060e6ad4dafaf1329c
GERISON KIMATHI <gerkim62@gmail.com>
2024-02-13 23:27:06 +0300

    error handling, make days from str to array

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("iCampus timetable scraper API is running...");
--- scrapeTimetable.js
+++ scrapeTimetable.js
@@ -48,149 +48,159 @@ async function scrapeTimetable(username, password) {
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
-  const browserLaunchEndTime = Date.now();
 
-  const loginStartTime = Date.now();
-  const page = await browser.newPage();
+  try {
+    const browserLaunchEndTime = Date.now();
 
-  await page.goto(ICAMPUS_HOME_URL);
+    const loginStartTime = Date.now();
+    const page = await browser.newPage();
 
-  await page.type(USERNAME_SELECTOR, username);
-  await page.click(AUTHENTICATE_BUTTON_SELECTOR);
+    await page.goto(ICAMPUS_HOME_URL);
 
-  // handle user does not exist here, done!
-  const errorMessage = await page.evaluate((ERROR_MESSAGE_SELECTOR) => {
-    return document.querySelector(ERROR_MESSAGE_SELECTOR)?.innerText;
-  }, ERROR_MESSAGE_SELECTOR);
+    await page.type(USERNAME_SELECTOR, username);
+    await page.click(AUTHENTICATE_BUTTON_SELECTOR);
 
-  console.log("Error message: ", errorMessage);
+    // handle user does not exist here, done!
+    const errorMessage = await page.evaluate((ERROR_MESSAGE_SELECTOR) => {
+      return document.querySelector(ERROR_MESSAGE_SELECTOR)?.innerText;
+    }, ERROR_MESSAGE_SELECTOR);
 
-  if (errorMessage) {
+    console.log("Error message: ", errorMessage);
+
+    if (errorMessage) {
+      await browser.close();
+      return {
+        error: {
+          code: errorMessage === USER_NOT_EXIST_ERROR_MESSAGE ? 404 : 500,
+          exists: true,
+          message: errorMessage,
+          possible_cause:
+            "The username you entered does not exist or is incorrect.",
+        },
+      };
+    }
+
+    const lockedOut = await page.evaluate((LOCKED_OUT_SELECTOR) => {
+      return document.querySelector(LOCKED_OUT_SELECTOR)?.innerText;
+    }, LOCKED_OUT_SELECTOR);
+
+    console.log("Locked out: ", lockedOut);
+    const currentUrl = page.url();
+    console.log("Current URL: ", currentUrl);
+
+    // Check if the account is locked
+
+    //   await page.waitForNavigation();
+    await page.waitForSelector(PASSWORD_SELECTOR);
+    await page.type(PASSWORD_SELECTOR, password);
+    await page.click(LOGIN_BUTTON_SELECTOR);
+
+    // await page.waitForSelector(INVALID_LOGIN_ERROR_SELECTOR);
+    await page.waitForNetworkIdle();
+
+    //   TODO: handle wrong password here
+    const invalidLoginError = await page.evaluate(
+      (INVALID_LOGIN_ERROR_SELECTOR) => {
+        return document.querySelector(INVALID_LOGIN_ERROR_SELECTOR)?.innerText;
+      },
+      INVALID_LOGIN_ERROR_SELECTOR
+    );
+
+    console.log("Invalid login error: ", invalidLoginError);
+    if (invalidLoginError) {
+      await browser.close();
+      return {
+        error: {
+          code: invalidLoginError
+            .trim()
+            .includes(INVALID_LOGIN_ERROR_MESSAGE.trim())
+            ? 401
+            : invalidLoginError
+                .trim()
+                .includes(CANT_VERIFY_USERNAME_ERROR_MESSAGE.trim())
+            ? 401
+            : 500,
+          exists: true,
+          message: invalidLoginError.replace("Go Home", ""),
+          possible_cause: invalidLoginError
+            .trim()
+            .includes(INVALID_LOGIN_ERROR_MESSAGE.trim())
+            ? "The password you entered is incorrect."
+            : invalidLoginError
+                .trim()
+                .includes(CANT_VERIFY_USERNAME_ERROR_MESSAGE.trim())
+            ? "You tried too many times."
+            : "The server might be experiencing some issues.",
+        },
+      };
+    }
+
+    // await page.waitForSelector(LOGOUT_BUTTON_SELECTOR); //This is to confirm that the user has logged in successfully
+    const loginEndTime = Date.now();
+    console.log("Logged in successfully");
+
+    const fetchTimetableStartTime = Date.now();
+    const timetablePageHtml = await page.evaluate(
+      async (ICAMPUS_TIMETABLE_URL) => {
+        const response = await fetch(ICAMPUS_TIMETABLE_URL);
+
+        return response.text();
+      },
+      ICAMPUS_TIMETABLE_URL
+    );
+    const fetchTimetableEndTime = Date.now();
+
+    const allTables = HtmlTableToJson.parse(timetablePageHtml);
+    console.log("All tables: ", allTables.results.length);
+
+    //noticed the last table is the actual timetable
+    const timetable = allTables.results[allTables.results.length - 1];
+
+    //   console.log(JSON.stringify(timetable, null, 2));
+
+    // Verify the shape of the timetable
+    const isTimetableValid = timetable.every(verifyCourseShape);
+
+    console.log("Is timetable valid: ", isTimetableValid);
+
+    if (!isTimetableValid) {
+      return {
+        error: {
+          exists: true,
+          code: 500,
+          message: "The timetable could not be scraped.",
+          possible_cause:
+            "Incorrect password or the server might be experiencing some issues.",
+        },
+      };
+    }
+
+    const result = {
+      timetable: timetable.map((course) => ({
+        ...course,
+        Days: course.Days.split(","),
+      })),
+      time_taken: {
+        browser_launch: browserLaunchEndTime - browserLaunchStartTime,
+        login: loginEndTime - loginStartTime,
+        fetch_timetable: fetchTimetableEndTime - fetchTimetableStartTime,
+      },
+      error: {
+        exists: false,
+      },
+    };
+
+    //   console.log(JSON.stringify(result, null, 2));
+
+    //CLOSE BROWSER
     await browser.close();
-    return {
-      error: {
-        code: errorMessage === USER_NOT_EXIST_ERROR_MESSAGE ? 404 : 500,
-        exists: true,
-        message: errorMessage,
-        possible_cause:
-          "The username you entered does not exist or is incorrect.",
-      },
-    };
-  }
 
-  const lockedOut = await page.evaluate((LOCKED_OUT_SELECTOR) => {
-    return document.querySelector(LOCKED_OUT_SELECTOR)?.innerText;
-  }, LOCKED_OUT_SELECTOR);
-
-  console.log("Locked out: ", lockedOut);
-  const currentUrl = page.url();
-  console.log("Current URL: ", currentUrl);
-
-  // Check if the account is locked
-
-  //   await page.waitForNavigation();
-  await page.waitForSelector(PASSWORD_SELECTOR);
-  await page.type(PASSWORD_SELECTOR, password);
-  await page.click(LOGIN_BUTTON_SELECTOR);
-
-  // await page.waitForSelector(INVALID_LOGIN_ERROR_SELECTOR);
-  await page.waitForNetworkIdle();
-
-  //   TODO: handle wrong password here
-  const invalidLoginError = await page.evaluate(
-    (INVALID_LOGIN_ERROR_SELECTOR) => {
-      return document.querySelector(INVALID_LOGIN_ERROR_SELECTOR)?.innerText;
-    },
-    INVALID_LOGIN_ERROR_SELECTOR
-  );
-
-  console.log("Invalid login error: ", invalidLoginError);
-  if (invalidLoginError) {
+    return result;
+  } catch (error) {
     await browser.close();
-    return {
-      error: {
-        code: invalidLoginError
-          .trim()
-          .includes(INVALID_LOGIN_ERROR_MESSAGE.trim())
-          ? 401
-          : invalidLoginError
-              .trim()
-              .includes(CANT_VERIFY_USERNAME_ERROR_MESSAGE.trim())
-          ? 401
-          : 500,
-        exists: true,
-        message: invalidLoginError.replace("Go Home", ""),
-        possible_cause: invalidLoginError
-          .trim()
-          .includes(INVALID_LOGIN_ERROR_MESSAGE.trim())
-          ? "The password you entered is incorrect."
-          : invalidLoginError
-              .trim()
-              .includes(CANT_VERIFY_USERNAME_ERROR_MESSAGE.trim())
-          ? "You tried too many times."
-          : "The server might be experiencing some issues.",
-      },
-    };
+
+    throw error;
   }
-
-  // await page.waitForSelector(LOGOUT_BUTTON_SELECTOR); //This is to confirm that the user has logged in successfully
-  const loginEndTime = Date.now();
-  console.log("Logged in successfully");
-
-  const fetchTimetableStartTime = Date.now();
-  const timetablePageHtml = await page.evaluate(
-    async (ICAMPUS_TIMETABLE_URL) => {
-      const response = await fetch(ICAMPUS_TIMETABLE_URL);
-
-      return response.text();
-    },
-    ICAMPUS_TIMETABLE_URL
-  );
-  const fetchTimetableEndTime = Date.now();
-
-  const allTables = HtmlTableToJson.parse(timetablePageHtml);
-  console.log("All tables: ", allTables.results.length);
-
-  //noticed the last table is the actual timetable
-  const timetable = allTables.results[allTables.results.length - 1];
-
-  //   console.log(JSON.stringify(timetable, null, 2));
-
-  // Verify the shape of the timetable
-  const isTimetableValid = timetable.every(verifyCourseShape);
-
-  console.log("Is timetable valid: ", isTimetableValid);
-
-  if (!isTimetableValid) {
-    return {
-      error: {
-        exists: true,
-        code: 500,
-        message: "The timetable could not be scraped.",
-        possible_cause:
-          "Incorrect password or the server might be experiencing some issues.",
-      },
-    };
-  }
-
-  const result = {
-    timetable,
-    time_taken: {
-      browser_launch: browserLaunchEndTime - browserLaunchStartTime,
-      login: loginEndTime - loginStartTime,
-      fetch_timetable: fetchTimetableEndTime - fetchTimetableStartTime,
-    },
-    error: {
-      exists: false,
-    },
-  };
-
-  //   console.log(JSON.stringify(result, null, 2));
-
-  //CLOSE BROWSER
-  await browser.close();
-
-  return result;
 }
 
 function verifyCourseShape(course) {
